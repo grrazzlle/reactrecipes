cc0dc3424f6dbc304e7a79894df42caa5e78766b
grrazzlle <cel47@miami.edu>
2021-06-21 12:34:50 -0400

    fix ingredients list seperation

--- client/src/components/RecipeList/index.js
+++ client/src/components/RecipeList/index.js
@@ -26,7 +26,7 @@ export function RecipeListItem({
           </Col>
           <Col size="xs-8 sm-9">
             <h3>{title}</h3>
-            <p>Ingredients: {ingredients}</p>
+            <p>Ingredients: {ingredients.join(', ')}</p>
             <a rel="noreferrer noopener" target="_blank" href={href}>
               Go to recipe!
             </a>
